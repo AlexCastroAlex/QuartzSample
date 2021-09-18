2e3ffb4097d02a9663b9cc17417642c7ba0b89ca
AlexCastro <alexandre.castro@exakis-nelite.com>
2021-09-18 09:29:29 +0200

    no message

--- QuartzSample/QuartzSample/Program.cs
+++ QuartzSample/QuartzSample/Program.cs
@@ -8,12 +8,15 @@ using QuartzSample;
 ISchedulerFactory schedulerFactory = new StdSchedulerFactory();            
 // 2. Get and start a scheduler
 IScheduler scheduler = await schedulerFactory.GetScheduler();
-await scheduler.Start();            // 3. Create a job
+await scheduler.Start();            
+// 3. Create a job
 IJobDetail job = JobBuilder.Create<NumberGeneratorJob>()
         .WithIdentity("number generator job", "number generator group")
-        .Build();            // 4. Create a trigger
+        .Build();            
+// 4. Create a trigger
 ITrigger trigger = TriggerBuilder.Create()
     .WithIdentity("number generator trigger", "number generator group")
     .WithSimpleSchedule(x => x.WithIntervalInSeconds(15).RepeatForever())
-    .Build();            // 5. Schedule the job using the job and trigger 
+    .Build();            
+// 5. Schedule the job using the job and trigger 
 await scheduler.ScheduleJob(job, trigger); Console.ReadLine();
