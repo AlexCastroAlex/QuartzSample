1da9503789562c6b226c197c0fbc415621983660
AlexCastro <alexandre.castro@exakis-nelite.com>
2021-09-18 09:20:48 +0200

    no message

---
+++ QuartzSample/QuartzSample/NumberGeneratorJob.cs
@@ -0,0 +1,24 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuartzSample
+{
+    class NumberGeneratorJob:IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            Console.WriteLine($"Your # is: {RandomNumber(1000, 9999)}");
+            await Task.CompletedTask;
+        }
+
+        private int RandomNumber(int min, int max)
+        {
+            var random = new Random();
+            return random.Next(min, max);
+        }
+    }
+}
